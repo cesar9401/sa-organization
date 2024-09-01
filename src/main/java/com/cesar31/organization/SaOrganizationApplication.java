042a74676ad40cb6403651f5d6afe4eeee1bba1c
César Tzoc <csartalva01@gmail.com>
2024-08-31 23:58:52 -0600

    Initial commit

---
+++ src/main/java/com/cesar31/organization/SaOrganizationApplication.java
@@ -0,0 +1,13 @@
+package com.cesar31.organization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaOrganizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaOrganizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesar31/organization/SaOrganizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar31.organization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaOrganizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
