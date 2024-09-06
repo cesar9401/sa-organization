0e803e65fb15b02e4ef2fb1c664c423d765be076
César Tzoc <csartalva01@gmail.com>
2024-09-05 20:11:45 -0600

    feat: adding default org

---
+++ src/main/resources/db/sa-organization/V6__default_org.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.sa_organization (organization_id, parent_organization_id, organization_name, organization_email, business_name, tax_identification_number, cat_organization_type, cat_status)
+VALUES ('421cab63-3b9f-406b-8270-4e32070384d9', null, 'ROOT', 'root@gmail.com', 'ROOT ORG', '7995996-3', 501, 514);
